9fd5be522f8059108eeb3ce5f5cb991d8e47d5cc
e-mimiko <emimiko@outlook.com>
2025-05-08 19:31:29 -0400

    create: microservice structure

---
+++ main.py
@@ -0,0 +1,6 @@
+# Author: Emiope Mimiko
+# GitHub username: e-mimiko
+# Date: 5/8/25
+# Description: #TODO
+# For #TODO: Add docstrings to all functions and classes
+
---
+++ server.py
@@ -0,0 +1,5 @@
+# Author: Emiope Mimiko
+# GitHub username: e-mimiko
+# Date: 5/8/25
+# Description: #TODO
+# For #TODO: Add docstrings to all functions and classes
